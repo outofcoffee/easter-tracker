e080fe5546e34dde8749b7b889606192ce806adc
Pete Cornish <outofcoffee@gmail.com>
2025-04-21 02:20:37 +0100

    feat: more facts.

--- src/data/easterFacts.ts
+++ src/data/easterFacts.ts
@@ -7,11 +7,11 @@ const easterFacts: EasterFact[] = [
   },
   {
     id: 2,
-    text: 'The largest Easter egg ever made was over 25 feet tall and weighed over 8,000 pounds!'
+    text: 'The largest Easter egg ever made was over 7.5 metres tall and weighed over 3,600 kilogrammes!'
   },
   {
     id: 3,
-    text: 'Americans buy more than 700 million marshmallow Peeps during Easter!'
+    text: 'The UK consumes more than 80 million chocolate Easter eggs annually!'
   },
   {
     id: 4,
@@ -30,7 +30,7 @@ const easterFacts: EasterFact[] = [
   },
   {
     id: 7,
-    text: 'In Finland, children plant grass seeds in small pots to symbolize new life and the coming of spring!',
+    text: 'In Finland, children plant grass seeds in small pots to symbolise new life and the coming of spring!',
     country: 'Finland'
   },
   {
@@ -53,7 +53,7 @@ const easterFacts: EasterFact[] = [
   },
   {
     id: 12,
-    text: 'The tradition of Easter eggs comes from the old idea that eggs represent new life!'
+    text: 'The tradition of Easter eggs comes from the ancient idea that eggs represent new life and rebirth!'
   },
   {
     id: 13,
@@ -62,7 +62,52 @@ const easterFacts: EasterFact[] = [
   },
   {
     id: 14,
-    text: 'Easter is named after Eostre, the ancient German goddess of spring!'
+    text: 'Easter is named after Eostre, the ancient Anglo-Saxon goddess of spring!'
+  },
+  {
+    id: 15,
+    text: 'In the UK, there is a custom of rolling decorated eggs down hills on Easter Monday!',
+    country: 'United Kingdom'
+  },
+  {
+    id: 16,
+    text: 'Hot cross buns are traditionally eaten on Good Friday in the UK!',
+    country: 'United Kingdom'
+  },
+  {
+    id: 17,
+    text: 'Simnel cake, a fruit cake with layers of marzipan, is a traditional Easter treat in the UK with 11 marzipan balls!',
+    country: 'United Kingdom'
+  },
+  {
+    id: 18,
+    text: 'The world\'s largest Easter egg hunt took place in London in 2012, with over 12,000 participants searching for eggs!',
+    country: 'United Kingdom'
+  },
+  {
+    id: 19,
+    text: 'In Scotland, Easter egg painting competitions and egg rolling events are popular community traditions!',
+    country: 'United Kingdom'
+  },
+  {
+    id: 20,
+    text: 'The British royal family traditionally attends Easter service at St George\'s Chapel in Windsor Castle!',
+    country: 'United Kingdom'
+  },
+  {
+    id: 21,
+    text: 'In Greece, eggs are dyed red and people play a game called tsougrisma, cracking their eggs against each other\'s!',
+    country: 'Greece'
+  },
+  {
+    id: 22,
+    text: 'In Greece, traditional Easter bread called tsoureki is baked with red-dyed eggs nestled in the dough!',
+    country: 'Greece'
+  },
+  {
+    id: 23,
+    text: 'On the Greek island of Corfu, people throw clay pots from their balconies on Easter Saturday!',
+    country: 'Greece'
   }
 ];
 
